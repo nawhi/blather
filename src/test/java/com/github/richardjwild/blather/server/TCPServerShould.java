23c6261e199faf1c698cfc7ef85a1d98b075e1fb
Nick <25394965+nawhi@users.noreply.github.com>
2019-09-18 11:25:15 +0100

    Use global timeout instead of test-specific

--- src/test/java/com/github/richardjwild/blather/server/TCPServerShould.java
+++ src/test/java/com/github/richardjwild/blather/server/TCPServerShould.java
@@ -1,6 +1,8 @@
 package com.github.richardjwild.blather.server;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 import java.io.IOException;
 
@@ -9,9 +11,12 @@ import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TCPServerShould {
 
+    @Rule
+    public Timeout timeout = Timeout.seconds(2);
+
     private static final int PORT = 8080;
 
-    @Test(timeout = 2000)
+    @Test
     public void print_welcome_message_from_single_client() throws IOException {
         TCPServer server = new TCPServer();
         server.initializeOn(PORT);
